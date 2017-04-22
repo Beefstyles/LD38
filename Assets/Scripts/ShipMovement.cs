7e14e4766bfdcfb46f65cd317085c2383925fc84
Beefstyles <glenwillis@gmail.com>
2017-04-22 20:34:49 +0800

    Transporting kinda works so will get back to it.

--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -8,11 +8,23 @@ public class ShipMovement : MonoBehaviour {
     public float MaxSpeed;
     public float RotationSpeed;
     public float MaxRotationSpeed;
+    public float ShipAltitude;
 
-	
+    
+
+	void Start()
+    {
+        ShipAltitude = transform.position.y;
+    }
+
 	void Update ()
     {
         transform.Translate(Vector3.forward * Speed);
         transform.Rotate(Vector3.up, RotationSpeed);
 	}
+
+    public void TransportShip(Vector3 TransportLocation)
+    {
+        transform.position = TransportLocation;
+    }
 }
---
+++ Assets/Scripts/TeleportShip.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportShip : MonoBehaviour {
+
+    public Transform TeleportLocation;
+    private Vector3 TransportLocation;
+    ShipMovement shipMovement;
+    
+    void Start()
+    {
+        shipMovement = FindObjectOfType<ShipMovement>();
+        
+    }
+	void OnTriggerEnter(Collider coll)
+    {
+        if(coll.gameObject.tag == "Ship")
+        {
+            TransportLocation = new Vector3(TeleportLocation.position.x, shipMovement.ShipAltitude, TeleportLocation.position.z);
+            Debug.Log("Moving ship");
+            shipMovement.TransportShip(TransportLocation);
+        }
+    }
+}
