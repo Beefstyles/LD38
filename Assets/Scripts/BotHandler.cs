b169e040efcb804e7d65c619e0888e124af92e81
Beefstyles <glenwillis@gmail.com>
2017-04-22 21:18:48 +0800

    Added a bot handler.

---
+++ Assets/Scripts/BotHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BotHandlerText
+{
+    public Text NumberMiningBotsTotalText, NumberMiningBotsActiveText, NumberMiningBotsPickupReqText, NumberExplorationBotsTotalText, NumberExplorationBotsActiveText, NumberExplorationBotsPickupReqText;
+    public Text NumberArchBotsTotalText, NumberArchBotsActiveText, NumberArchBotsPickupReqText;
+}
+public class BotHandler : MonoBehaviour {
+
+
+    public int NumberMiningBotsTotal;
+    public int NumberMiningBotsActive;
+    public int NumberMiningBotsPickupReq;
+    public int NumberExplorationBotsTotal;
+    public int NumberExplorationBotsActive;
+    public int NumberExplorationBotsPickupReq;
+    public int NumberArchBotsTotal;
+    public int NumberArchBotsActive;
+    public int NumberArchBotsPickupReq;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/ShipInformation.cs
+++ Assets/Scripts/ShipInformation.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ShipInformation : MonoBehaviour {
 
+    [HideInInspector]
     public string GridLocation;
 	// Use this for initialization
 	void Start () {
