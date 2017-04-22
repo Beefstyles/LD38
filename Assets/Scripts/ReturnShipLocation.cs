7d630f8af126b17b9bd7f134d4ef6a09a488a5f3
Beefstyles <glenwillis@gmail.com>
2017-04-22 15:37:38 +0800

    Finally getting started.

---
+++ Assets/Scripts/ReturnShipLocation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnShipLocation : MonoBehaviour {
+
+    public string GridLocation;
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if(coll.tag == "Ship")
+        {
+            Debug.Log("You have entered " + GridLocation);
+        }
+    }
+
+    void OnTriggerExit(Collider coll)
+    {
+        if (coll.tag == "Ship")
+        {
+            Debug.Log("You have exited " + GridLocation);
+        }
+    }
+}
