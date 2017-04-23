fbd5a65394f9eab0a9165175bce42cf83b8739b0
Beefstyles <glenwillis@gmail.com>
2017-04-23 22:20:59 +0800

    More buying.

--- Assets/Scripts/EquipmentUpgradeHandler.cs
+++ Assets/Scripts/EquipmentUpgradeHandler.cs
@@ -10,12 +10,16 @@ public class EquipmentUpgradeHandler : MonoBehaviour {
 
     public int PassiveH3IronCost, PassiveH3GoldCost, PassiveH3PlatCost, PassiveH3CarbonCost;
     public int NewMiningBotIronCost, NewMiningBotGoldCost, NewMiningBotPlatCost, NewMiningBotCarbonCost;
+    public int NewArchIronCost, NewArchBotGoldCost, NewArchBotPlatCost, NewArchBotCarbonCost;
+    public int NewExplorBotIronCost, NewExplorBotGoldCost, NewExplorBotPlatCost, NewExplorBotCarbonCost;
 
     ResourceInformation resourceInfo;
+    BotHandler botHandler;
 
     void Start()
     {
         resourceInfo = FindObjectOfType<ResourceInformation>();
+        botHandler = FindObjectOfType<BotHandler>();
     }
 
     public void ReceiveBuyingButton(BuyingOptions buyOption)
@@ -25,17 +29,50 @@ public class EquipmentUpgradeHandler : MonoBehaviour {
             case BuyingOptions.H3Unit:
                 if (resourceInfo.TotalIron > PassiveH3IronCost && resourceInfo.TotalGold > PassiveH3GoldCost && resourceInfo.TotalPlatinum > PassiveH3PlatCost && resourceInfo.TotalCarbon > PassiveH3CarbonCost)
                 {
-
+                    resourceInfo.NumberPassiveH3Units++;
+                }
+                else
+                {
+                    BuyingFailed();
                 }
                     break;
             case BuyingOptions.NewMiningBot:
+                if (resourceInfo.TotalIron > NewMiningBotIronCost && resourceInfo.TotalGold > NewMiningBotGoldCost && resourceInfo.TotalPlatinum > NewMiningBotPlatCost && resourceInfo.TotalCarbon > NewMiningBotCarbonCost)
+                {
+                    botHandler.NoMiningBotsTotal++;
+                }
+                else
+                {
+                    BuyingFailed();
+                }
                 break;
             case BuyingOptions.NewArchBot:
+                if (resourceInfo.TotalIron > NewArchIronCost && resourceInfo.TotalGold > NewArchBotGoldCost && resourceInfo.TotalPlatinum > NewArchBotPlatCost && resourceInfo.TotalCarbon > NewArchBotCarbonCost)
+                {
+                    botHandler.NoArchBotsTotal++;
+                }
+                else
+                {
+                    BuyingFailed();
+                }
                 break;
             case BuyingOptions.NewExplorBot:
+                if (resourceInfo.TotalIron > NewExplorBotIronCost && resourceInfo.TotalGold > NewExplorBotGoldCost && resourceInfo.TotalPlatinum > NewExplorBotPlatCost && resourceInfo.TotalCarbon > NewExplorBotCarbonCost)
+                {
+                    botHandler.NoExplorationBotsTotal++;
+                }
+                else
+                {
+                    BuyingFailed();
+                }
                 break;
             default:
                 break;
         }
+    }
+
+    public void BuyingFailed()
+    {
+
     }
 }
