e943a837abc9ed1231633481c15dca6338deb594
Beefstyles <glenwillis@gmail.com>
2017-04-24 22:06:21 +0800

    Have a heads up display for grid location.

---
+++ Assets/Scripts/DisplayCurrentShipLocation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayCurrentShipLocation : MonoBehaviour {
+
+    public TextMeshPro ShipLocation;
+    ShipInformation shipInfo;
+
+	void Start ()
+    {
+        shipInfo = FindObjectOfType<ShipInformation>();
+    }
+	
+	void Update () {
+        ShipLocation.text = shipInfo.GridLocation;
+    }
+}
